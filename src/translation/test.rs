7d98b4445ab86a269df92f950f39b6b4e9d62c34
Morgan Bentell <morgan.bentell@gmail.com>
2022-02-04 09:19:40 +0100

    scaling and rotation

--- src/translation/test.rs
+++ src/translation/test.rs
@@ -1,12 +1,12 @@
 #[cfg(test)]
 mod translation {
-    use crate::translation::translation;
+    use crate::translation::{rotation_x, rotation_y, rotation_z, scaling, translation};
     use crate::tuple::{point, vector};
+    use std::f32::consts::PI;
 
     #[test]
     fn multiplying_by_translation_matrix() {
         let transform = translation(5.0, -3.0, 2.0);
-
         let p = point(-3.0, 4.0, 5.0);
 
         assert_eq!(p * transform, point(2.0, 1.0, 7.0));
@@ -16,7 +16,6 @@ mod translation {
     fn multiplying_with_inverse_of_translation_matrix() {
         let transform = translation(5.0, -3.0, 2.0);
         let inv = transform.inverse();
-
         let p = point(-3.0, 4.0, 5.0);
 
         assert_eq!(p * inv, point(-8.0, 7.0, 3.0));
@@ -25,9 +24,92 @@ mod translation {
     #[test]
     fn translation_does_not_affect_vectors() {
         let transform = translation(5.0, -3.0, 2.0);
-
         let v = vector(-3.0, 4.0, 5.0);
 
         assert_eq!(v * transform, v);
+    }
+
+    #[test]
+    fn scaling_matrix_applied_to_point() {
+        let scaling = scaling(2.0, 3.0, 4.0);
+        let p = point(-4.0, 6.0, 8.0);
+
+        assert_eq!(p * scaling, point(-8.0, 18.0, 32.0));
+    }
+
+    #[test]
+    fn scaling_matrix_applied_to_vector() {
+        let scaling = scaling(2.0, 3.0, 4.0);
+        let v = vector(-4.0, 6.0, 8.0);
+
+        assert_eq!(v * scaling, vector(-8.0, 18.0, 32.0));
+    }
+
+    #[test]
+    fn multiplying_with_invers_of_scaling_matrix() {
+        let s = scaling(2.0, 3.0, 4.0);
+        let inv_s = s.inverse();
+        let v = vector(-4.0, 6.0, 8.0);
+
+        assert_eq!(v * inv_s, vector(-2.0, 2.0, 2.0));
+    }
+
+    #[test]
+    fn reflection_by_scaling_with_negative() {
+        let s = scaling(-1.0, 1.0, 1.0);
+        let p = vector(2.0, 3.0, 4.0);
+
+        assert_eq!(p * s, vector(-2.0, 3.0, 4.0));
+    }
+
+    #[test]
+    fn rotating_point_around_x_axis() {
+        let p = point(0.0, 1.0, 0.0);
+        let half_quarter = rotation_x(PI / 4.0);
+        let full_quarter = rotation_x(PI / 2.0);
+
+        assert_eq!(
+            p * half_quarter,
+            point(0.0, f32::sqrt(2.0) / 2.0, f32::sqrt(2.0) / 2.0)
+        );
+        assert_eq!(p * full_quarter, point(0.0, 0.0, 1.0));
+    }
+
+    #[test]
+    fn inverse_of_x_rotation_rotates_in_opposite_direction() {
+        let p = point(0.0, 1.0, 0.0);
+        let half_quarter = rotation_x(PI / 4.0);
+        let inv = half_quarter.inverse();
+
+        assert_eq!(
+            p * inv,
+            point(0.0, f32::sqrt(2.0) / 2.0, -f32::sqrt(2.0) / 2.0)
+        )
+    }
+
+    #[test]
+    fn rotating_point_around_y_axis() {
+        let p = point(0.0, 0.0, 1.0);
+        let half_quarter = rotation_y(PI / 4.0);
+        let full_quarter = rotation_y(PI / 2.0);
+
+        assert_eq!(
+            p * half_quarter,
+            point(f32::sqrt(2.0) / 2.0, 0.0, f32::sqrt(2.0) / 2.0)
+        );
+        assert_eq!(p * full_quarter, point(1.0, 0.0, 0.0));
+    }
+
+    #[test]
+    fn rotating_point_around_z_axis() {
+        let p = point(0.0, 1.0, 0.0);
+        let half_quarter = rotation_z(PI / 4.0);
+        let full_quarter = rotation_z(PI / 2.0);
+
+        assert_eq!(
+            p * half_quarter,
+            point(-f32::sqrt(2.0) / 2.0, f32::sqrt(2.0) / 2.0, 0.0)
+        );
+        assert_eq!(p * full_quarter, point(-1.0, 0.0, 0.0));
     }
 }
--- src/translation/translation.rs
+++ src/translation/translation.rs
@@ -1,10 +1,33 @@
 use crate::matrix::Matrix;
 
 pub fn translation(x: f32, y: f32, z: f32) -> Matrix {
-    let mut t = Matrix::identity();
-    t = t.set(0, 3, x);
-    t = t.set(1, 3, y);
-    t = t.set(2, 3, z);
+    Matrix::identity().set(0, 3, x).set(1, 3, y).set(2, 3, z)
+}
 
-    t
+pub fn scaling(x: f32, y: f32, z: f32) -> Matrix {
+    Matrix::identity().set(0, 0, x).set(1, 1, y).set(2, 2, z)
+}
+
+pub fn rotation_x(rad: f32) -> Matrix {
+    Matrix::identity()
+        .set(1, 1, f32::cos(rad))
+        .set(1, 2, -f32::sin(rad))
+        .set(2, 1, f32::sin(rad))
+        .set(2, 2, f32::cos(rad))
+}
+
+pub fn rotation_y(rad: f32) -> Matrix {
+    Matrix::identity()
+        .set(0, 0, f32::cos(rad))
+        .set(0, 2, f32::sin(rad))
+        .set(2, 0, -f32::sin(rad))
+        .set(2, 2, f32::cos(rad))
+}
+
+pub fn rotation_z(rad: f32) -> Matrix {
+    Matrix::identity()
+        .set(0, 0, f32::cos(rad))
+        .set(0, 1, -f32::sin(rad))
+        .set(1, 0, f32::sin(rad))
+        .set(1, 1, f32::cos(rad))
 }
