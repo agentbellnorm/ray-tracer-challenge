8cacf05d53f38d11d9d819b233c756e89bf9058d
Morgan Bentell <morgan.bentell@gmail.com>
2022-06-30 08:56:28 +0200

    add shiny scene

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ use crate::lights::PointLight;
 use crate::material::Material;
 use crate::matrix::Matrix;
 use crate::pattern::Pattern;
-use crate::scenes::{maggies_world, shiny_scene};
+use crate::scenes::{debug_glass, maggies_world, shiny_scene};
 use crate::shapes::Shape;
 use crate::transformation::view_transformation;
-use crate::tuple::{point, vector};
+use crate::tuple::{point, point_i, vector, vector_i};
 use crate::world::World;
-use std::f64::consts::{FRAC_PI_2, FRAC_PI_3, FRAC_PI_4, FRAC_PI_8};
+use std::f64::consts::{FRAC_PI_2, FRAC_PI_3, FRAC_PI_4, FRAC_PI_8, PI};
 use std::process::Command;
 
 mod camera;
@@ -33,13 +33,16 @@ mod world;
 
 fn main() {
     let world = World::with(
-        maggies_world(),
+        shiny_scene(),
         PointLight::with(point(-10.0, 10.0, -10.0), white()),
     );
 
-    let camera = Camera::new(1920, 1080, FRAC_PI_3).set_transform(view_transformation(
-        point(0.0, 1.5, -5.0),
-        point(0.0, 1.0, 0.0),
+    // let (horizontal, vertical) = (1280, 720);
+    let (horizontal, vertical) = (1920, 1080);
+
+    let camera = Camera::new(horizontal, vertical, FRAC_PI_3).set_transform(view_transformation(
+        point(0.0, 2.8, -5.0),
+        point(0.0, 1.5, 0.0),
         vector(0.0, 1.0, 0.0),
     ));
 
--- src/scenes/shiny.rs
+++ src/scenes/shiny.rs
@@ -1,24 +1,131 @@
 use crate::{rgb, white, Material, Matrix, Pattern, Shape};
 use std::f64::consts::FRAC_PI_2;
 
+use rand::rngs::StdRng;
+use rand::{Rng, SeedableRng};
+
+const SEED: u64 = 8122325293;
+
+const PASTELS: [[i32; 3]; 6] = [
+    [49, 191, 243],
+    [164, 132, 233],
+    [244, 136, 154],
+    [255, 175, 104],
+    [246, 230, 131],
+    [121, 212, 94],
+];
+
+const BILLIARDS: [[i32; 3]; 8] = [
+    [0, 0, 0],
+    [255, 0, 0],
+    [0, 255, 0],
+    [0, 0, 255],
+    [255, 0, 255],
+    [255, 255, 0],
+    [0, 255, 255],
+    [165, 42, 42],
+];
+
+pub fn glass_balls() -> Vec<Shape> {
+    let size = 1.5;
+    let ball = Shape::sphere_glass().with_transform(
+        Matrix::identity()
+            .scale(size, size, size)
+            .translate(1.3, size, 6.0),
+    );
+
+    vec![ball]
+}
+
+pub fn chrome_balls() -> Vec<Shape> {
+    let size = 1.0;
+    let ball = Shape::sphere_chrome().with_transform(
+        Matrix::identity()
+            .scale(size, size, size)
+            .translate(-2.8, size, 4.0),
+    );
+
+    vec![ball]
+}
+
+pub fn billiard_balls() -> Vec<Shape> {
+    let mut rng = StdRng::seed_from_u64(SEED);
+    let mut v = vec![];
+    for i in 0..8 {
+        let rnd_c = BILLIARDS[i];
+        let mut material = Material::from_color(rgb(rnd_c[0], rnd_c[1], rnd_c[2]));
+        material.shininess = 300.0;
+        material.specular = 1.0;
+        material.diffuse = 0.3;
+        material.reflective = 0.1;
+        material.ambient = 0.3;
+
+        let ball_size = 0.6;
+        let ball = Shape::sphere_from_material(material).with_transform(
+            Matrix::identity()
+                .scale(ball_size, ball_size, ball_size)
+                .translate(
+                    rng.gen_range(-50..50) as f64 / 10.0,
+                    ball_size,
+                    rng.gen_range(-40..=100) as f64 / 10.0,
+                ),
+        );
+        v.push(ball)
+    }
+
+    v
+}
+
+pub fn pastel_balls() -> Vec<Shape> {
+    let mut rng = StdRng::seed_from_u64(SEED);
+    let mut v = vec![];
+    for _ in 0..100 {
+        let rnd_c = PASTELS[rng.gen_range(0..6)];
+        let mut material = Material::from_color(rgb(rnd_c[0], rnd_c[1], rnd_c[2]));
+        material.shininess = 1.0;
+        material.specular = 0.0;
+        material.ambient = 0.3;
+
+        let ball_size = 0.4;
+        let ball = Shape::sphere_from_material(material).with_transform(
+            Matrix::identity()
+                .scale(ball_size, ball_size, ball_size)
+                .translate(
+                    rng.gen_range(-150..=150) as f64 / 10.0,
+                    ball_size,
+                    rng.gen_range(-40..=400) as f64 / 10.0,
+                ),
+        );
+        v.push(ball)
+    }
+
+    v
+}
+
 pub fn shiny_scene() -> Vec<Shape> {
-    let mut ground_materal = Material::from_color(rgb(255, 182, 193));
-    ground_materal.diffuse = 0.9;
-    ground_materal.specular = 0.1;
-    let ground = Shape::plane_from_material(ground_materal);
+    let mut ground_material = Material::from_color(white());
+    ground_material.diffuse = 0.9;
+    ground_material.specular = 0.1;
+    ground_material.ambient = 0.5;
+    let ground = Shape::plane_from_material(ground_material);
 
     let mut sky_material = Material::from_pattern(
         Pattern::gradient(rgb(135, 206, 250), white()).with_transformation(Matrix::identity()),
     );
     sky_material.diffuse = 0.9;
-    sky_material.specular = 0.1;
+    sky_material.specular = 0.01;
     let sky = Shape::plane_from_material(sky_material).with_transform(
         Matrix::identity()
-            .scale(20.0, 1.0, 1.0)
+            .scale(35.0, 1.0, 1.0)
             .rotate_x(FRAC_PI_2)
             .rotate_z(FRAC_PI_2)
-            .translate(0.0, 0.0, 40.0),
+            .translate(0.0, 0.0, 60.0),
     );
 
-    vec![ground, sky]
+    let mut objects = vec![ground, sky];
+    objects.extend(glass_balls());
+    objects.extend(pastel_balls());
+    objects.extend(chrome_balls());
+    objects.extend(billiard_balls());
+    objects
 }
