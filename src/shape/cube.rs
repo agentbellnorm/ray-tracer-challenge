577b77556f2b608b8b3bc74b17aceab325f6a6ec
Morgan Bentell <morgan.bentell@gmail.com>
2022-07-14 13:59:51 +0200

    handle scenario when ray does not intersect cube

--- src/shape/cube.rs
+++ src/shape/cube.rs
@@ -22,6 +22,10 @@ pub fn cube_intersects(ray: &Ray) -> Vec<f64> {
     let t_min = xt_min.max(yt_min).max(zt_min);
     let t_max = xt_max.min(yt_max).min(zt_max);
 
+    if t_min > t_max {
+        return vec![];
+    }
+
     vec![t_min, t_max]
 }
 
@@ -29,7 +33,7 @@ pub fn cube_intersects(ray: &Ray) -> Vec<f64> {
 mod cube_test {
     use crate::rays::Ray;
     use crate::tuple::Tuple;
-    use crate::tuple::{point, vector_i};
+    use crate::tuple::{point, point_i, vector, vector_i};
     use crate::Shape;
     use parameterized::parameterized;
 
@@ -49,5 +53,18 @@ mod cube_test {
         assert_eq!(xs.len(), 2, "{}", scenario);
         assert_eq!(xs.get(0).t, t1, "{}", scenario);
         assert_eq!(xs.get(1).t, t2, "{}", scenario);
+    }
+
+    #[parameterized(
+    origin = {      point_i(-2, 0, 0),              point_i(0, -2, 0),              point_i(0, 0, -2),                point_i(2, 0, 2),   point_i(0, 2, 2),   point_i(2, 2, 0)    },
+    direction = {   vector(0.2673, 0.5345, 0.8018), vector(0.8018, 0.2673, 0.5345), vector(0.5345, 0.8018, 0.2673), vector_i(0, 0, -1), vector_i(0, -1, 0), vector_i(-1, 0, 0)  }
+    )]
+    pub fn ray_misses_cube(origin: Tuple, direction: Tuple) {
+        let cube = Shape::cube_default();
+        let ray = Ray::with(origin, direction);
+
+        let xs = cube.intersects(&ray);
+
+        assert_eq!(xs.len(), 0)
     }
 }
